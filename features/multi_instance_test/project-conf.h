e87000b4655d9697dd86575a7cceb8c86458c983
Kevin Andrea <KevinAndrea@users.noreply.github.com>
2021-07-12 14:42:20 -0700

    Adding first pass files for multi-inst testing.

---
+++ features/multi_instance_test/project-conf.h
@@ -0,0 +1,6 @@
+// Overriders for rpl-conf.h (RPL Configuration)
+#define RPL_CONF_MOP RPL_MOP_STORING_MULTICAST  // Storing-Mode with MCast
+
+// ???
+#define PROCESS_CONF_NO_PROCESS_NAMES 1
+#define UIP_CONF_TCP 0
