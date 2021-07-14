494c2d95687cedb8961bb9292db2f6a5cec7ed94
Kevin Andrea <KevinAndrea@users.noreply.github.com>
2021-07-14 16:26:17 -0700

    Updating viewconf for RPL_NBR_POLICY_MAX_NEXTHOP_NEIGHBORS

--- features/multi_instance_test/project-conf.h
+++ features/multi_instance_test/project-conf.h
@@ -1,6 +1,6 @@
 // Overriders for rpl-conf.h (RPL Configuration)
 #define RPL_CONF_MOP RPL_MOP_STORING_MULTICAST  // Storing-Mode with MCast
+#define RPL_CONF_WITH_DAO_ACK 1                 // Enables DAO-ACK Msgs
 
-// ???
-#define PROCESS_CONF_NO_PROCESS_NAMES 1
-#define UIP_CONF_TCP 0
+//  Legacy
+#define PROCESS_CONF_NO_PROCESS_NAMES 1         // Saves RAM - No Proc Names Stored
